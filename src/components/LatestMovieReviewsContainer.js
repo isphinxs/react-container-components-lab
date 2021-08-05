d1bc91ca926aa96d98f481870910390ef6a139a3
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-04 21:16:45 -0400

    commit tree c26bafb5262b31b1fd14746d6fc58e9d485b3234

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = process.env.REACT_APP_NYT_API_KEY;
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+            .then(resp => resp.json())
+            .then(reviews => {
+                // console.log(reviews);
+                this.setState({reviews: reviews.results});
+                // debugger;
+                console.log(this.state);
+            })
+    }
+
+    render() {
+        // debugger;
+        // const reviews = this.state.reviews.map(review => <MovieReviews review={review} />);
+        return (
+            <div className="latest-movie-reviews">
+                <h3>Latest Movie Reviews</h3>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer;
